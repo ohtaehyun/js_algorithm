d2588375297d79d9ca6ea6649b828e7ad2f0e4fc
ohtaehyun <othdev95@gmail.com>
2022-04-30 10:00:25 +0900

    조금 더 깔끔하게 수정

--- programmers/shuttleBus.js
+++ programmers/shuttleBus.js
@@ -1,8 +1,7 @@
 // https://programmers.co.kr/learn/courses/30/lessons/17678
 function solution(n, t, m, timetable) {
     const shuttleTable = [];
-    const startTime = 9*60;
-    
+    const startTime = 9*60;  
     for(let i = 0; i < n; i++){
         shuttleTable.push(startTime+i*t);
     }
@@ -12,8 +11,7 @@ function solution(n, t, m, timetable) {
         return Number(splited[0])*60 + Number(splited[1]);
     }).sort((a,b)=>a-b);
     
-    const crewTable = [];
-    
+    const crewInBus = [];
     let cnt = 0;
     for(let i = 0; i < n; i++){
         const ct = [];
@@ -21,19 +19,16 @@ function solution(n, t, m, timetable) {
             ct.push(timetable[j]);
         }
         cnt += ct.length;
-        crewTable.push(ct);
+        crewInBus.push(ct);
     }
     
     let answer = 0;
     
-    if(crewTable[n-1].length < m){
+    if(crewInBus[n-1].length < m){
         answer = shuttleTable[n-1];
     }
     else{
-        if(crewTable[n-1][m-1] === crewTable[n-1][0]){
-            answer = crewTable[n-1][0]-1;
-        }
-        answer = Math.max(...crewTable[n-1])-1;
+        answer = Math.max(...crewInBus[n-1])-1;
     }
     
     const hour = Math.floor(answer / 60);
