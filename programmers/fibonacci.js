ee28aa1f6d262e3ac83cf4af75b85009b5dee820
ohtaehyun <othdev95@gmail.com>
2022-05-22 23:38:41 +0900

    [프로그래머스] 피보나치의 수
    간단한 dp구현

---
+++ programmers/fibonacci.js
@@ -0,0 +1,9 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12945
+function solution(n) {
+    const dp = new Array(n+1).fill(0);
+    dp[1] = 1;
+    for(let i = 2; i <= n; i++){
+        dp[i] = (dp[i-2] + dp[i-1])%1234567; 
+    }
+    return dp[n];
+}
